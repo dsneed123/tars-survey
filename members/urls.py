2f304c0ef906ba95d17d80f1f8cdf2ce10bdace8
Davis Sneed <101902819+dsneed123@users.noreply.github.com>
2026-04-23 11:58:08 -0700

    Fix scrolling behavior in chat feed (#57)
    Co-authored-by: dsneed123 <davissneed@Daviss-Mac-mini.local>

--- members/urls.py
+++ members/urls.py
@@ -8,5 +8,6 @@ app_name = "members"
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("dashboard/quick-task/", views.quick_add_task, name="quick_task"),
+    path("dashboard/messages/", views.load_more_messages, name="load_more_messages"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
